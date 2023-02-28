dfc9c3f7213cee146df527b1d350a46977b63ad3
Isaiah Espinoza <gespinoz@umd.edu>
2023-02-28 14:51:50 -0500

    added figures and images folder; edited national anthems R script; included bar plot

--- national anthems.R
+++ national anthems.R
@@ -6,10 +6,11 @@ library(broom)
 library(tidyr)
 library(textdata)
 
-anthem <- read.csv("anthems.csv")
+anthem <- rio::import("data-raw/anthems.csv") %>% 
+  janitor::clean_names()
 
 tidy_anthem <- anthem %>%
-  unnest_tokens(word, Anthem)
+  unnest_tokens(word, anthem)
 
 tidy_anthem <- tidy_anthem %>%
   anti_join(stop_words)
@@ -25,34 +26,50 @@ saveRDS(anthem_sentiment, "anthem_sentiments.rds")
 
 anthem_sentiment_continent <- tidy_anthem %>%
   inner_join(get_sentiments("bing"), by="word") %>%
-  count(Continent, sentiment) %>%
+  count(continent, sentiment) %>%
   pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>% 
   mutate(sentiment = positive - negative)
 
 saveRDS(anthem_sentiment_continent, "anthem_sentiments continent.rds")
 
 
-p2 <- ggplot(anthem_sentiment_continent, aes(Continent, sentiment, fill = Continent)) +
+p2 <- ggplot(anthem_sentiment_continent, aes(continent, sentiment, fill = continent)) +
   geom_col(show.legend = FALSE) +
-  facet_wrap(~Continent, ncol = 2, scales = "free_x")
-
+  facet_wrap(~continent, ncol = 2, scales = "free_x")
 
 nrc_anthem_sentiment_continent <- tidy_anthem %>%
   inner_join(get_sentiments("nrc"), by="word") %>%
-  count(Continent, sentiment) %>%
+  count(continent, sentiment) %>% 
   pivot_wider(names_from = sentiment, values_from = n, values_fill = 0)
 
-df2 <- data.frame(t(nrc_anthem_sentiment_continent[-1]))
-colnames(df2) <- continent_names
-
-continent_names <- c("Africa","Asia","Europe", "North America","Oceania",
-                     "South America")
-
+# make another data frame of the NRC sentiment stuff in long format
+nrc_senti_long <- tidy_anthem %>%
+  inner_join(get_sentiments("nrc"), by="word") %>%
+  count(continent, sentiment)
 
 
-p3 <- ggplot(anthem_sentiment_continent, aes(, fill = Continent)) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~continent_names, ncol = 2, scales = "free_x")
+# convert sentiment variable from character class to factor 
+nrc_senti_long$sentiment <- nrc_senti_long$sentiment %>% 
+  unique() %>%
+  as_factor()
+
+# check to see if all 10 levels of sentiment are in factor
+levels(nrc_senti_long$sentiment)
+
+# create facet plot of raw counts of nrc sentiment by continent
+p3 <- nrc_senti_long %>%
+  ggplot(aes(x = continent, y = n))+
+  geom_col() + 
+  scale_x_discrete(guide = guide_axis(angle = 45))+ # this angles the x-axis labels by 45 degrees
+  facet_wrap(vars(sentiment), scales = "free_x")
+
+# save as pdf in 'figures' folder
+ggsave('nrc_sentiment_by_continent_counts.pdf',
+       plot = p3,
+       device = "pdf",
+       path = "~/R/national_anthems/figures/")
+
+
 
 ggplot(data=df2, aes(x=dose, y=len, fill=supp)) +
   geom_bar(stat="identity", position=position_dodge())
