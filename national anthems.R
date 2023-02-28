2c47e7535634abbfbccea6a4acf351724ae16eca
J-Dewar <jordanldewar@gmail.com>
2023-02-28 12:36:04 -0500

    doin stuff

--- national anthems.R
+++ national anthems.R
@@ -7,7 +7,13 @@ library(tidyr)
 
 anthem <- read.csv("anthems.csv")
 
-anthem_sentiment <- anthem_tidy %>%
+tidy_anthem <- anthem %>%
+  unnest_tokens(word, Anthem)
+
+tidy_anthem <- tidy_anthem %>%
+  anti_join(stop_words)
+
+anthem_sentiment <- tidy_anthem %>%
   inner_join(get_sentiments("bing"), by="word") %>%
   count(document, sentiment) %>%
   pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>% 
